730b94de5936732cc7897516ec5a74c4f830d1e1
Andrej Mišustin <andrey.mishustin@iflix.com>
2019-01-25 14:59:17 +0100

    extra comment

--- src/web-automation/app-factory.js
+++ src/web-automation/app-factory.js
@@ -5,6 +5,9 @@ module.exports = class AppFactory {
   // Theoretically your eshop could have a non-web application, for example Android Mobile app.
   // The same user acceptance test suite could be executed against web and mobile app
   // as long as testing App class implements the same interface
+
+  // Frankly I'm not the big fan of the UI-driven testing,
+  // I would probably start with API-level testing, but let's discuss that at F2F interview
   static getApp() {
     /* if (config.says.mobile) {
         // https://webdriver.io/docs/api/appium.html
